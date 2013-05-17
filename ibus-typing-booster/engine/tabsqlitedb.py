mike-fabian/ibus-typing-booster

commit 996cebb7e6d625a42202536994a81d382575972d

The “compare” function to sort the candidates in select_words() should be local to select_words()
This function is only used to sort the candidates returned by select_words().
It should not be a member function of the whole “tabsqlitedb” class.
Either make it local to select_words() or use a lambda function.

ibus-typing-booster/engine/tabsqlitedb.py
@@ -407,12 +407,6 @@ class tabsqlitedb:
         if commit:
             self.db.commit()
 
-    def compare (self,x,y):
-#        return cmp (x[1], y[1]) or -(cmp (x[-1], y[-1])) \
-#                or -(cmp (x[-2], y[-2])) or (cmp (x[0], y[0]))
-        return -(cmp (x[-1], y[-1])) or (cmp (x[1], y[1])) \
-                or -(cmp (x[-2], y[-2])) or (cmp (x[0], y[0]))
-
     def select_words(self, input_phrase):
         '''
         Get phrases from database by tab_key objects
@@ -450,7 +444,12 @@ class tabsqlitedb:
         _cand = mudb.values()
         map(_cand.append, filter(lambda x: x, map(lambda key: key not in mudb and usrdb[key], usrdb)))
         map(_cand.append, filter(lambda x: x, map(lambda key: key not in mudb and key not in usrdb and sysdb[key], sysdb)))
-        _cand.sort(cmp=self.compare)
+        _cand.sort(cmp=(lambda x,y:
+                        -(cmp(x[-1], y[-1]))    # user_freq descending
+                        or (cmp(x[1], y[1]))    # len(input_phrase) ascending
+                        or -(cmp(x[-2], y[-2])) # freq descending
+                        or (cmp(x[0], y[0]))    # id ascending
+                    )
         return _cand[:]
 
 

commit 63a7788fd0997fe024ce86dcd970736bd70f422a

Fix syntax (missing “)”) in select_words()
Error was introduced by

commit 996cebb7e6d625a42202536994a81d382575972d
Author: Mike FABIAN <mfabian@redhat.com>
Date:   Wed May 15 00:49:49 2013 +0200

    The “compare” function to sort the candidates in select_words() should be local to select_words()

Sorry.

ibus-typing-booster/engine/tabsqlitedb.py
@@ -429,7 +429,7 @@ class tabsqlitedb:
                         or (cmp(x[1], y[1]))    # len(input_phrase) ascending
                         or -(cmp(x[-2], y[-2])) # freq descending
                         or (cmp(x[0], y[0]))    # id ascending
-                    )
+                    ))
         return _cand[:]
 
 