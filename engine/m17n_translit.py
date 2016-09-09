7688eae61161937d4e8b5c19f0b36a98f4ca1f55
Mike FABIAN <mfabian@redhat.com>
2016-09-09 14:39:35 +0200

    Fix file headers
    The file headers still mentioned ibus-table, but this is not ibus-table anymore.
    
    Update copyright entries and make format of headers more consistent.

--- engine/m17n_translit.py
+++ engine/m17n_translit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:et sts=4 sw=4
 #
-# ibus-typing-booster - The Tables engine for IBus
+# ibus-typing-booster - A completion input method for IBus
 #
 # Copyright (c) 2015-2016 Mike FABIAN <mfabian@redhat.com>
 #
@@ -9,11 +9,13 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
+#
+# You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 '''A module to do transliteration using m17n-lib.
--- engine/tabsqlitedb.py
+++ engine/tabsqlitedb.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # vim:et sts=4 sw=4
 #
-# ibus-typing-booster - The Tables engine for IBus
+# ibus-typing-booster - A completion input method for IBus
 #
 # Copyright (c) 2011-2013 Anish Patil <apatil@redhat.com>
-# Copyright (c) 2012-2013 Mike FABIAN <mfabian@redhat.com>
+# Copyright (c) 2012-2016 Mike FABIAN <mfabian@redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
