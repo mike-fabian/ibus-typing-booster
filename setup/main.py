f5464878a718e19ca47e7d21838921fe29956fc9
Mike FABIAN <mfabian@redhat.com>
2024-04-09 12:30:30 +0200

    Decrease grid_row_spacing from 5 to 1 pixels to make the setup tool still fit on a 1024x768 pixel screen

--- setup/main.py
+++ setup/main.py
@@ -216,7 +216,7 @@ class SetupUI(Gtk.Window): # type: ignore
         self._dialog_action_area.add(self._close_button)
 
         grid_border_width = 10
-        grid_row_spacing = 5
+        grid_row_spacing = 1
         grid_column_spacing = 10
 
         self._dictionaries_and_input_methods_vbox = Gtk.Box()
